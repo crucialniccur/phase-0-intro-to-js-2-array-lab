2cdec6da6201aed9187ca02d47c6956f7b9c2f9f
Mati Joseph <crucialniccur@gmail.com>
2025-02-25 21:49:32 +0300

    describe two functions , removeFirstCat and removeLastCat

--- index.js
+++ index.js
@@ -25,3 +25,10 @@ function appendCat(name) {
 function prependCat(name) {
   return [name, ...cats];
 }
+
+function removeLastCat(name) {
+  return cats.slice(0, -1);
+}
+function removeFirstCat(name) {
+  return cats.slice(1);
+}
